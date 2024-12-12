895f9df7dc101121b31080307a715298ff457af4
mickaelgall <mickaelgall6@gmail.com>
2024-12-12 20:09:38 +0100

    v8

--- image.php
+++ image.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<?php 
+require_once 'connexion.php';
 
-</head>
-<body>
+INSERT INTO livre (photo, noauteur, titre);
+VALUES ('romeo et juliette.jpg','1','Roméo et Juliette');
 
-<div class="container mt-3">
-  <img src="bibliotequeminecraft.jpg" alt="bibliothèque " display="inline-block" height="200" class="rounded-3"></span>
-</div>
-
-</body>
-</html>
+?>
