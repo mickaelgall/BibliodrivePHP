0f277fcd8901852cc5bc10cbe0e911eae02f8236
mickaelgall <mickaelgall6@gmail.com>
2025-01-03 20:20:02 +0100

    v33

--- barre_recherche_admin.php
+++ barre_recherche_admin.php
@@ -3,19 +3,19 @@
 Vous ètes a l'espace administrateur, je m'appelle Mickaël Gall, bienvenue dans ma bibliodrive !
     <br> <!--espace de ligne-->
     Le site est en maintenance jusqu'au 10 janvier 2025, merci de votre visite.
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark form-control me-2">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-warning form-control me-2">
       <div class="container-fluid">
-        <a class="navbar-brand" href="http://127.0.0.1/PHP/BibliodrivePHP/accueil_admin.php">accueil administrateur<img src=".\image_site\table-enchantement-minecraft.jpg" alt="bibliothèque " width="50" height="50"></a>
+        <a class="navbar-brand text-dark" href="http://127.0.0.1/PHP/BibliodrivePHP/accueil_admin.php">accueil administrateur<img src=".\image_site\table-enchantement-minecraft.jpg" alt="bibliothèque " width="50" height="50"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="mynavbar">
           <ul class="navbar-nav me-auto">
             <li class="nav-item">
-              <a class="nav-link" href="http://127.0.0.1/PHP/BibliodrivePHP/page_ajouter_un_livre_admin.php">ajouter un livre<img src=".\image_site\pioche_minecraft_or.jpg" alt="bibliothèque " width="35" height="35"></a>
+              <a class="nav-link text-dark" href="http://127.0.0.1/PHP/BibliodrivePHP/page_ajouter_un_livre_admin.php">ajouter un livre<img src=".\image_site\pioche_minecraft_or.jpg" alt="bibliothèque " width="35" height="35"></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="http://127.0.0.1/PHP/BibliodrivePHP/page_Créer_un _membre_admin.php">Créer un membre<img src=".\image_site\pioche_minecraft_diament.jpg" alt="bibliothèque " width="35" height="35"></a>
+              <a class="nav-link text-dark" href="http://127.0.0.1/PHP/BibliodrivePHP/page_Créer_un _membre_admin.php">Créer un membre<img src=".\image_site\pioche_minecraft_diament.jpg" alt="bibliothèque " width="35" height="35"></a>
             </li>
         </div>
       </div>
